bffdc506f9190b2212f5154a6a09fc5b217f4295
henderjon <list@box370.net>
2024-06-14 15:15:26 -0500

    renames log_writer.go

--- log_writer.go
+++ log_writer.go
@@ -10,6 +10,7 @@ import (
 // 	stdout  = LogWriter{log.New(os.Stdout, "", 0)}
 // )
 
+// LogWriter is a simple wrapper around log.Logger that adds the io.Writer interface
 type LogWriter struct {
 	*log.Logger
 }
